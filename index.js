738b3b717ed7ea4d55cfdfc0f28877287d991b71
Daniel White <danielfwhite1@gmail.com>
2020-05-29 20:30:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let price1 = 5;
+let price2 = 12;
+
+let wallet1 = 250;
+let wallet2 = 100;
+
+let friends1 = 120;
+let friends2 = 75;
+
+let age1 = 20;
+let age2 = 25;
+
+let first1 = 'Sophia';
+let first2 = 'Danny';
+
+let last1 = 'Krafcik';
+let last2 = 'White';
+
+let middle1 = 'F';
+let middle2 = 'F';
+
+let newWallet1 = wallet1 - price1;
+let newWallet2 = wallet2 - price2;
+
+let newFriends1 = friends1 / age1;
+let newFriends2 = friends2 / age2;
+
+let fullName1 = first1 + " " + middle1 + " " + last1;
+let fullName2 = first2 + " " + middle2 + " " + last2;
+
+console.log("New amount of money in Sophia's wallet is: " + newWallet1);
+console.log("New amount of money in Danny's wallet is: " + newWallet2);
+console.log("Number of friends per year for Sophia is: " + newFriends1);
+console.log("Number of friends per year for Danny is: " + newFriends2);
+console.log("Full name for Sophia is: " + fullName1);
+console.log("Full name for Danny is: " + fullName2);
